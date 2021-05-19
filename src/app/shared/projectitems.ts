f68f1173a82331360298900c6d70bb8df51c74ef
David Yuan <davidy804@gmail.com>
2021-05-19 18:19:55 -0400

    fix details

--- src/app/shared/projectitems.ts
+++ src/app/shared/projectitems.ts
@@ -13,7 +13,7 @@ export const ProjectItems: ProjectModel[] = [
                 caption: 'This website is being hosted by Github Pages. The website was built using the Angular framework. In order to build the site using Angular, the files need to be outputted to a /docs folder. This was the command that was ran to create the folder with the necessary files.',
             },
             {
-                imgSrc: './asssets/images/portfolio2.png',
+                imgSrc: './assets/images/portfolio2.png',
                 caption: 'This is the html for the projects component. It pulls information from other files to decrease clutter and increase scalability.',
             },
             {
